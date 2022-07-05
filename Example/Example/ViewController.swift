e0ad949e6b8b5f85bae6e1ba8ed734c686ab4d90
Mehran Kamalifard <mehran.kmlf@icloud.com>
2022-07-05 23:04:18 +0430

    Refactor podspec file

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Mehran on 4/14/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
