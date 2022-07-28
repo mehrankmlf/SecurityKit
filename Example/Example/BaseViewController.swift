aa5acd1c25e1e92e117e8145fce216dba2913f40
Mehran Kamalifard <mehran.kmlf@icloud.com>
2022-07-28 10:04:11 +0430

    Improve ecample project

---
+++ Example/Example/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  Example
+//
+//  Created by Mehran on 5/6/1401 AP.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
